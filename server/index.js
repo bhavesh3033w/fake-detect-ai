bdf1905f454f643de7a48b117609383ae8b2bc3b
bhavesh malviya <bhaveshmalviya@bhaveshs-MacBook-Pro.local>
2026-04-10 17:43:08 +0530

    temporary fix cors

--- server/index.js
+++ server/index.js
@@ -17,16 +17,7 @@ const allowedOrigins = [
   "https://fake-detect-ai.vercel.app"
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
